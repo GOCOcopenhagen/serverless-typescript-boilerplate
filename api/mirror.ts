fc2712729493d025caf590849a4896f3418786a1
Anton Tobias Jensen <41019970+Statsministeriet@users.noreply.github.com>
2021-03-05 14:24:00 +0100

    Update mirror function

--- api/mirror.ts
+++ api/mirror.ts
@@ -1,12 +1,12 @@
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import handler from "libs/handler-lib";
+import { response } from "libs/responeTypes";
 
-export async function main(event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v2.0! Your function executed successfully!. Local vaiable: "+process.env.SAMPLE_ENV_VAR,
-      context,
-      event,
-    }),
-  };
-}
+
+export const get = handler(async (event: APIGatewayEvent, context: Context): Promise<response> => {
+  return ({
+    message: "Go Serverless v2.0! Your function executed successfully!. Local vaiable: "+process.env.SAMPLE_ENV_VAR,
+    context,
+    event,
+  })
+})
