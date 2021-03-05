8af1ce00b0299679578730f3e22d5ae5efee8a75
Anton Tobias Jensen <41019970+Statsministeriet@users.noreply.github.com>
2021-03-05 14:25:59 +0100

    Remove unused import

--- api/mirror.ts
+++ api/mirror.ts
@@ -1,9 +1,9 @@
-import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { Context, APIGatewayEvent } from "aws-lambda";
 import handler from "libs/handler-lib";
 import { response } from "libs/responeTypes";
 
 
-export const get = handler(async (event: APIGatewayEvent, context: Context): Promise<response> => {
+export const main = handler(async (event: APIGatewayEvent, context: Context): Promise<response> => {
   return ({
     message: "Go Serverless v2.0! Your function executed successfully!. Local vaiable: "+process.env.SAMPLE_ENV_VAR,
     context,
